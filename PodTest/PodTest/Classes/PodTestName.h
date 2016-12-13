bdff993f7b10826247fd432c1cc9cf9e38c26036
wangsuyan <wsyxyxs@126.com>
2016-12-13 11:01:57 +0800

    修饰了一点小内容，没必要添加注释（wsy）

---
+++ PodTest/PodTest/Classes/PodTestName.h
@@ -0,0 +1,13 @@
+//
+//  PodTestName.h
+//  PodTest
+//
+//  Created by WangSuyan on 2016/12/13.
+//  Copyright © 2016年 WangSuyan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PodTestName : NSObject
+
+@end
---
+++ PodTest/PodTest/Classes/PodTestName.m
@@ -0,0 +1,13 @@
+//
+//  PodTestName.m
+//  PodTest
+//
+//  Created by WangSuyan on 2016/12/13.
+//  Copyright © 2016年 WangSuyan. All rights reserved.
+//
+
+#import "PodTestName.h"
+
+@implementation PodTestName
+
+@end
